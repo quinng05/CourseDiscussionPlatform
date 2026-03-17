6218d827f2ac2893b5c10ce7f87d72c48eea3564
quinng05 <quinng05@vt.edu>
2026-03-17 17:51:14 -0400

    Fixed comments

--- db/sample_data.sql
+++ db/sample_data.sql
@@ -11,7 +11,7 @@
 # TRUNCATE TABLE rating;
 # SET FOREIGN_KEY_CHECKS = 1;
 
-# This is adding to user
+-- This is adding to user
 INSERT INTO user
 VALUES (1, "mmle04@vt.edu", "Michael", NOW(), "student"),
 (2, "gvwilson@vt.edu", "Grant", NOW(), "student"),
@@ -30,7 +30,7 @@ VALUES (1, "mmle04@vt.edu", "Michael", NOW(), "student"),
 (15, "sysadmin5@vt.edu", "sysadmin5", NOW(), "sysadmin");
 
 
-# This is adding to courses
+-- This is adding to courses
 INSERT INTO course
 VALUES ("CHEM1035", "General Chemistry", "Course on General Chemistry", 3),
 ("ENGL1105", "First-Year Writing", "Course on First-Year Writing", 3),
@@ -38,19 +38,19 @@ VALUES ("CHEM1035", "General Chemistry", "Course on General Chemistry", 3),
 ("CS3214", "Computer Systems", "Computer System Course", 3),
 ("CS4604", "Int Data Base Mgt Sys", "Database Course", 3);
 
-# This is adding to student
+-- This is adding to student
 INSERT INTO student
 VALUES (1, "ENGL"), (2, "MATH"), (3, "CS"), (4, "CS"), (5, "CHEM");
 
-# This is adding to teacher
+-- This is adding to teacher
 INSERT INTO teacher
 VALUES (6, "CS"), (7, "CS"), (8, "MATH"), (9, "ENGL"), (10, "CHEM");
 
-# This is adding to sysadmin
+-- This is adding to sysadmin
 INSERT INTO sysadmin
 VALUES (11), (12), (13), (14), (15);
 
-# This is adding to semester
+-- This is adding to semester
 INSERT INTO semester
 VALUES (1, "Fall", 2025, "2025-08-24", "2025-12-09"),
 (2, "Spring", 2025, "2025-01-20", "2025-05-06"),
@@ -58,7 +58,7 @@ VALUES (1, "Fall", 2025, "2025-08-24", "2025-12-09"),
 (4, "Summer", 2026, "2026-05-26", "2026-07-06"),
 (5, "Fall", 2026, "2026-08-24", "2026-12-09");
 
-# This is adding to courseinstructor
+-- This is adding to courseinstructor
 INSERT INTO courseinstructor
 VALUES (1, "CHEM1035", 10, 11),
 (2, "ENGL1105", 9, 11),
@@ -66,7 +66,7 @@ VALUES (1, "CHEM1035", 10, 11),
 (4, "CS3214", 6, 13),
 (5, "CS4604", 7, 14);
 
-# This is adding to discussionpost
+-- This is adding to discussionpost
 INSERT INTO discussionpost
 VALUES (1, "This class sucks", "2025-12-02", 1, 1, 1, 1),
 (2, "I loved the way this class was taught", NOW(), 5, 3, 2, 2),
@@ -74,7 +74,7 @@ VALUES (1, "This class sucks", "2025-12-02", 1, 1, 1, 1),
 (4, "I enjoyed learning in this class", "2025-11-28", 5, 1, 3, 4),
 (5, "I found the class to be boring", "2026-03-16", 3, 3, 4, 5);
 
-# This is adding to discussionpost
+-- This is adding to discussionpost
 INSERT INTO rating
 VALUES (1, 1, "Hate this class", "2025-12-02", 1, 1, 1),
 (2, 5, "Great Class", NOW(), 2, 5, 3),
